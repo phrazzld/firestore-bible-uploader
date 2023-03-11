9f4d1c89f00f863406f6aaccbe21650dd2c534d0
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2023-03-10 21:53:03 -0600

    Add embeddings

--- createRawVerses.js
+++ createRawVerses.js
@@ -1,5 +1,13 @@
+const { Configuration, OpenAIApi } = require("openai");
 const admin = require("firebase-admin");
 const fs = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 // Initialize Firestore and authentication credentials
 /* const serviceAccount = require("./service-account-key.json"); */
@@ -9,51 +17,101 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 
-// Read the contents of the text file
-const text = fs.readFileSync("./kjv.txt", "utf-8");
+const embedBlob = async (blob) => {
+  console.log(`Embedding ${blob}`);
+  const response = await openai.createEmbedding({
+    model: "text-embedding-ada-002",
+    input: blob,
+  });
+  const embeddings = response.data.data[0].embedding;
+  return embeddings;
+};
 
-// Split the text into an array of lines
-const lines = text.split("\n");
+const addRawVerses = async () => {
+  // Read the contents of the text file
+  const text = fs.readFileSync("./kjv.txt", "utf-8");
 
-// Define a regular expression to match book names
-const bookRegex = /^BOOK:(.+)/;
+  // Split the text into an array of lines
+  const lines = text.split("\n");
 
-// Define a regular expression to match chapter and verse numbers
-const chapterVerseRegex = /^(\d+):(\d+)/;
+  // Define a regular expression to match book names
+  const bookRegex = /^BOOK:(.+)/;
 
-// Define the identifier for the KJV Bible document
-const kjvDocId = "kjv";
+  // Define a regular expression to match chapter and verse numbers
+  const chapterVerseRegex = /^(\d+):(\d+)/;
 
-// Write the Bible to Firestore
-// Create the rawVerses collection if it doesn't exist
-const rawVersesRef = db.collection("rawVerses");
+  // Define the identifier for the KJV Bible document
+  const kjvDocId = "kjv";
 
-let book = null;
+  // Write the Bible to Firestore
+  // Create the rawVerses collection if it doesn't exist
+  const rawVersesRef = db.collection("rawVerses");
 
-for (let i = 0; i < lines.length; i++) {
-  const line = lines[i];
-  const bookMatch = line.match(bookRegex);
-  if (bookMatch) {
-    book = bookMatch[1].trim();
-    // Set the book name
-  } else {
-    const chapterVerseMatch = line.match(chapterVerseRegex);
-    if (chapterVerseMatch) {
-      const chapter = chapterVerseMatch[1].trim();
-      const verse = chapterVerseMatch[2].trim();
-      const verseText = line
-        .substring(chapterVerseMatch[0].length + 1)
-        .trim();
-      // Set the chapter, verse, and verse text
-      const data = {
-        book,
-        chapter,
-        verse,
-        text: verseText,
-        bible: kjvDocId.trim(),
-        blob: `${book.trim()} ${chapter.trim()}:${verse.trim()} ${verseText.trim()}`
-      };
-      rawVersesRef.add(data);
+  let book = null;
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const bookMatch = line.match(bookRegex);
+    if (bookMatch) {
+      book = bookMatch[1].trim();
+      // Set the book name
+    } else {
+      const chapterVerseMatch = line.match(chapterVerseRegex);
+      if (chapterVerseMatch) {
+        const chapter = chapterVerseMatch[1].trim();
+        const verse = chapterVerseMatch[2].trim();
+        const verseText = line
+          .substring(chapterVerseMatch[0].length + 1)
+          .trim();
+        const blob = `${book.trim()} ${chapter.trim()}:${verse.trim()} ${verseText.trim()}`;
+
+        // Check if the document exists
+        // It doesn't have a predictable ID, so we have to query it
+        const query = await rawVersesRef
+          .where("book", "==", book)
+          .where("chapter", "==", chapter)
+          .where("verse", "==", verse)
+          .get();
+
+        if (query.empty) {
+          console.log(`Adding ${book} ${chapter}:${verse}`);
+          // If the document doesn't exist, create the embedding and add the document
+          const vec = await embedBlob(blob);
+          const data = {
+            book,
+            chapter,
+            verse,
+            text: verseText,
+            bible: kjvDocId.trim(),
+            blob,
+            vec,
+          };
+          rawVersesRef.add(data);
+        } else {
+          console.log(
+            `Document for ${book} ${chapter}:${verse} already exists, checking embeddings...`
+          );
+          // If the document already exists, check if it has a vec field that's an array with length 1536
+          const doc = query.docs[0];
+          const data = doc.data();
+          if (data.vec && Array.isArray(data.vec) && data.vec.length === 1536) {
+            console.log(
+              `Document for ${book} ${chapter}:${verse} already has an embedding, skipping...`
+            );
+            // If it does, skip this verse
+            continue;
+          } else {
+            console.log(
+              `Document for ${book} ${chapter}:${verse} doesn't have an embedding, updating...`
+            );
+            // If it doesn't, create the embedding and update the document
+            const vec = await embedBlob(blob);
+            await doc.ref.update({ vec });
+          }
+        }
+      }
     }
   }
-}
+};
+
+addRawVerses();
